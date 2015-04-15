ec181181e666d4a01a5ff49f19be4ba30284ea7e
maugan <maugan.legourrierec@gmail.com>
2015-04-15 23:56:14 +0200

    2015-05-15

--- Automate.java
+++ Automate.java
@@ -9,67 +9,75 @@ import java.util.List;
 
 public class Automate
 {
-  private String A;//alphabet
-  private int e[];//Etats
-  private int ei;//etat initial
-  private int f[];//etats finaux 
-  private List<Transition> delta;//ensemble des transitions
-  
-  private int etati;//etat dans lequel est l'automate
+  //alphabet
+  private final String A;
+  //Etats
+  private final List<Integer> e;
+  //etat initial
+  private final int ei;
+  //etats finaux
+  private final List<Integer> f;
+  //ensemble des transitions
+  private final List<Transition> delta;
+  //etat dans lequel est l'automate
+  private int etati;
    
-
-  public Automate(String A,int e[],int f[],List<Transition> delta)
+  public Automate(String A,List<Integer> e,List<Integer> f,List<Transition> delta)
   {
-	  
     this.A=A;
-    this.e=e;
+    this.e=new ArrayList<Integer>(e);
+    this.f=new ArrayList<Integer>(f);
+    this.delta=new ArrayList<Transition>(delta);
     this.ei=0;
-    this.f=f;
-    delta=new ArrayList(delta);
   }
   
-  public String getAlphabet()
-  {
-    return A;
-  }
+  public String getAlphabet() { return A;}
   
-  public int[] getEtat()
-  {
-    return e;
-  }
+  public List<Integer> getEtat() { return e;}
    
-  public int[] getEtatsFinaux()
+  public List<Integer> getEtatsFinaux() { return f;}
+  
+  public List<Transition> getTransition(){ return delta;}
+  
+  @Override
+  public String toString()
   {
-    return f;
+    StringBuilder sb = new StringBuilder();
+    return sb.append("alphabet : ").append(A).append("\n")
+             .append("Etats : ").append(e).append("\n")
+             .append("Etat initial : ").append(ei).append("\n")
+             .append("Etat finaux : ").append(f).append("\n")
+             .append("transitions : ").append(delta)
+             .toString();
   }
   
-  public List<Transition> getTransition()
+  @Override
+  public boolean equals(Object obj)
   {
-    return delta;
+     if (obj==this) { return true;}
+     if (obj==null) { return false;}
+     
+     if (obj instanceof Automate) 
+     {
+       Automate t = (Automate) obj;
+       
+       if(A.equals(t.A) && e.equals(t.e) && f.equals(t.f) && delta.equals(t.delta))
+         return true;
+     }
+     
+     return false;
   }
-  
-  /*public boolean ifInAlphabet(String s)
-  {
-    for(int i=0;i<s.length();i++)
-    {
-      switch(s.charAt(i))
-      {
-        
-        
-        
-        default:
-            return false;
-      }
-    }
-    
-    return true;
-  }*/
   
   public static void main(String []args)
   {
 	 String alphab="ab";
-	 int etat[] = {0,1,2,3};
-	 int etatF[] = {3};
+	 List<Integer> etat = new ArrayList<Integer>();
+   etat.add(0);
+   etat.add(1);
+   etat.add(2);
+   etat.add(3);
+	 List<Integer> etatF = new ArrayList<Integer>();
+   etatF.add(3);
 	 List<Transition> transition=new ArrayList<Transition>();
 	 transition.add(new Transition(0,'a',0));
 	 transition.add(new Transition(0,'b',0));
@@ -78,11 +86,7 @@ public class Automate
 	 transition.add(new Transition(2,'b',3));
 	 
 	 Automate a = new Automate(alphab,etat,etatF,transition);
-     
-     System.out.println(a.getAlphabet());
-     System.out.println(a.getEtat());
-     System.out.println(a.getEtatsFinaux());
-     System.out.println(a.getTransition());
-     
+	 
+   System.out.println(a);
   }
 }
--- Transition.java
+++ Transition.java
@@ -1,4 +1,4 @@
-public class Transition<E>
+public class Transition
 {
   private int previousState;
   private char transition;
@@ -6,8 +6,42 @@ public class Transition<E>
   
   public Transition(int pStat,char trans,int nState)
   {
-	previousState = pStat;
-	transition = trans;
-	nextState = nState;
+	  previousState = pStat;
+	  transition = trans;
+	  nextState = nState;
   }
+  
+  @Override
+  public String toString()
+  {
+    StringBuilder sb = new StringBuilder();
+    return sb.append("(").append(previousState).append(",").append(transition).append(",").append(nextState).append(")").toString();
+  }
+  
+  @Override
+  public boolean equals(Object obj)
+  {
+     if (obj==this) { return true;}
+     if (obj==null) { return false;}
+     
+     if (obj instanceof Transition) 
+     {
+       Transition t = (Transition) obj;
+       
+       if(t.previousState == this.previousState && t.transition == this.transition && t.nextState == this.nextState)
+         return true;
+     }
+     
+     return false;
+  }
+  
+   public static void main(String []args)
+   {
+     Transition t = new Transition(0,'a',5);
+     Transition s = new Transition(2,'b',3);
+     
+     
+     System.out.println(t);
+     System.out.println(s);
+   }
 }
